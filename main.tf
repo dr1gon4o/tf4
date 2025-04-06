6217239ad7c6c5229ef46815afb04ffc250ed121
dragon <basselt@abv.bg>
2025-04-06 18:02:58 +0300

    4.24

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-      # version = "4.25.0"
+      # version = "~> 4.0"
+      version = "4.24.0"
     }
   }
 }
